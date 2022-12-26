c94e04e06b77a2700f6779d4538c5a98b8cb2de7
socpa linda <linda.socpa@institutsaintjean.org>
2022-12-26 10:19:14 +0100

    modified setting

--- Best_Deal/settings.py
+++ Best_Deal/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+
 ]
 
 ROOT_URLCONF = 'Best_Deal.urls'
