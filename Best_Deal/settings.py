bc3628d97d53ffe7c6a492aa79749031d3e19f5f
socpa linda <linda.socpa@institutsaintjean.org>
2022-12-26 10:29:08 +0100

    modified setting

--- Best_Deal/settings.py
+++ Best_Deal/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'deals',
-     "whitenoise.runserver_nostatic",
-    "django.contrib.staticfiles",
+
 ]
 
 MIDDLEWARE = [
